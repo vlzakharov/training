43991593000ac03753f9aa11169d010bc4824641
vlzakharov <vl.zakharovdd@gmail.com>
2024-04-04 19:08:34 +0300

    Create install-nginx.sh

---
+++ install-nginx.sh
@@ -0,0 +1,23 @@
+#Установка обновлений
+clear
+echo Устанавливаем обновления
+sleep 1
+apt-get update
+
+#Установка nginx сервера
+clear
+echo Устанавливаем nginx сервер
+sleep 1
+apt-get install -y nginx
+
+#Запуск nginx сервера на yc виртуальной машине
+clear
+echo Запуск nginx сервера на yc виртуальной машине
+sleep 1
+service nginx start
+
+#Проверяем работоспособность в виртуальной машине
+clear
+echo Проверка работоспособности
+sleep 1
+systemctl status nginx.service
